a00641526988bbfa03a84bddaa8da62469345617
khyatisaini <sainikhyati2001@gmail.com>
2021-06-29 13:26:47 +0530

    popup menu for track card

--- lib/widgets/tracksScrrenWidgets/trackCard.dart
+++ lib/widgets/tracksScrrenWidgets/trackCard.dart
@@ -12,67 +12,126 @@ class TrackCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: () {
-        Navigator.of(context).pushNamed(
-          TrackDetailsScreen.routeName,
-          arguments: track,
-        );
-      },
-      child: Container(
-        width: deviceWidth * 0.9,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                color: kSpringColor,
-                borderRadius: BorderRadius.only(topLeft: Radius.circular(deviceWidth * 0.018), topRight: Radius.circular(deviceWidth * 0.018)),
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Container(
-                    padding: EdgeInsets.symmetric(
-                      vertical: 20,
-                      horizontal: 20,
-                    ),
-                    child: RichText(
-                      text: TextSpan(
-                        text: 'Track ',
-                        style: TextStyle(
-                          fontWeight: FontWeight.w800,
-                          color: kWhiteColor
-                        ),
-                        children: [
-                          TextSpan(
-                            text: track.begin.toUtc().toString().replaceFirst('.000Z', ''),
-                          ),
-                        ]
-                      ),
-                    ),
+    return Container(
+      width: deviceWidth * 0.9,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: kSpringColor,
+              borderRadius: BorderRadius.only(topLeft: Radius.circular(deviceWidth * 0.018), topRight: Radius.circular(deviceWidth * 0.018)),
+            ),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 20,
+                    horizontal: 20,
                   ),
-                  Container(
-                    padding: EdgeInsets.all(15),
+                  child: RichText(
+                    text: TextSpan(
+                      text: 'Track ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w800,
+                        color: kWhiteColor,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: track.begin.toUtc().toString().replaceFirst('.000Z', ''),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: PopupMenuButton(
+                    enabled: true,
                     child: Icon(
                       Icons.more_vert_outlined,
                       color: kWhiteColor,
                     ),
-                  )
-                ],
-              ),
+                    onSelected: (int index) {
+                      if (index == 0) {
+                        Navigator.of(context).pushNamed(
+                          TrackDetailsScreen.routeName,
+                          arguments: track,
+                        );
+                      }
+                      else if (index == 1) {
+                        // TODO: function to delete track
+                        debugPrint('delete track tapped');
+                      }
+                      else if (index == 2) {
+                        // TODO: function to upload track as open data
+                        debugPrint('upload as open data tapped');
+                      }
+                      else if (index == 3) {
+                        // TODO: function to export track
+                        debugPrint('export track tapped');
+                      }
+                    },
+                    itemBuilder: (_) => [
+                      PopupMenuItem(
+                        child: Text(
+                          'Show Details',
+                        ),
+                        value: 0,
+                      ),
+                      PopupMenuItem(
+                        child: Text(
+                          'Delete Track',
+                        ),
+                        value: 1,
+                      ),
+                      PopupMenuItem(
+                        child: Text(
+                          'Upload Track as Open Data',
+                        ),
+                        value: 2,
+                      ),
+                      PopupMenuItem(
+                        child: Text(
+                          'Export Track',
+                        ),
+                        value: 3,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            Container(
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                TrackDetailsScreen.routeName,
+                arguments: track,
+              );
+            },
+            // TODO: replace the placeholder map image with map widget
+            child: Container(
               child: Image.asset(
                 'assets/images/map_placeholder.png',
-                width: double.infinity,
-                height: deviceHeight * 0.2,
                 fit: BoxFit.cover,
+                height: deviceHeight * 0.2,
+                width: double.infinity,
               ),
             ),
-            Container(
+          ),
+          GestureDetector(
+            onTap: () {
+              Navigator.of(context).pushNamed(
+                TrackDetailsScreen.routeName,
+                arguments: track,
+              );
+            },
+            child: Container(
               padding: EdgeInsets.symmetric(
                 vertical: 20,
                 horizontal: 20,
@@ -94,6 +153,9 @@ class TrackCard extends StatelessWidget {
                           fontWeight: FontWeight.w800,
                         ),
                       ),
+                      // SizedBox(
+                      //   height: deviceHeight * 0.02,
+                      // ),
                       Text(
                         'Duration',
                         style: TextStyle(
@@ -114,6 +176,9 @@ class TrackCard extends StatelessWidget {
                           fontWeight: FontWeight.w800,
                         ),
                       ),
+                      // SizedBox(
+                      //   height: deviceHeight * 0.02,
+                      // ),
                       Text(
                         'Distance',
                         style: TextStyle(
@@ -126,20 +191,20 @@ class TrackCard extends StatelessWidget {
                 ],
               ),
             ),
-          ],
-        ),
-        decoration: BoxDecoration(
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey[350],
-              blurRadius: 3.0,
-              spreadRadius: 1.0,
-              offset: Offset(-2, 2),
-            ),
-          ],
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(5)),
-        ),
+          ),
+        ],
+      ),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[350],
+            blurRadius: 3.0,
+            spreadRadius: 1.0,
+            offset: Offset(-2, 2),
+          ),
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(5)),
       ),
     );
   }
