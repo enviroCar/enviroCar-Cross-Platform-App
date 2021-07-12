a8d57e664d42aad6aabc41c093591e384a6ad45b
D <dajayk12@gmail.com>
2021-07-12 22:51:40 +0530

    Added logger

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:logger/logger.dart';
 
 import './dashboardScreen.dart';
 import './profileScreen.dart';
@@ -15,6 +17,12 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
+  final Logger _logger = Logger(
+    printer: PrettyPrinter(
+      printTime: true,
+    ),
+  );
+
   int _currentIndex = 0;
 
   List<Widget> _screensList;
@@ -38,6 +46,7 @@ class _IndexState extends State<Index> {
   }
 
   void showHelpDialog() {
+    _logger.i('Showing help dialog');
     showDialog(
       context: context,
       builder: (_) {
@@ -81,6 +90,7 @@ class _IndexState extends State<Index> {
           children: [
             GestureDetector(
               onTap: () {
+                _logger.i('On dashboard');
                 changeScreen(0);
               },
               child: SvgPicture.asset(
@@ -91,6 +101,7 @@ class _IndexState extends State<Index> {
             ),
             GestureDetector(
               onTap: () {
+                _logger.i('On tracks screen');
                 changeScreen(1);
               },
               child: SvgPicture.asset(
@@ -101,6 +112,7 @@ class _IndexState extends State<Index> {
             ),
             GestureDetector(
               onTap: () {
+                _logger.i('On settings screen');
                 changeScreen(2);
               },
               child: Icon(
@@ -111,6 +123,7 @@ class _IndexState extends State<Index> {
             ),
             GestureDetector(
               onTap: () {
+                _logger.i('On profile screen');
                 changeScreen(3);
               },
               child: Icon(
