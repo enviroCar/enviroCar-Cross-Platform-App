fbd99c6de7e5e22f895224df069bf8597ef1e448
khyatisaini <sainikhyati2001@gmail.com>
2021-08-10 18:03:11 +0530

    add status indicator widget for indicating bluetooth connection and location status on track screen

---
+++ lib/animations/rippleAnimation.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+import '../constants.dart';
+import '../utils/circlePainter.dart';
+
+class RippleAnimation extends StatefulWidget {
+  final double size;
+  final Widget child;
+
+  const RippleAnimation({this.size, this.child});
+
+  @override
+  _RippleAnimationState createState() => _RippleAnimationState();
+}
+
+class _RippleAnimationState extends State<RippleAnimation> with TickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: const Duration(milliseconds: 2000),
+      vsync: this
+    )..repeat();
+  }
+
+  @override
+  void dispose() {
+    animationController.stop();
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: CustomPaint(
+          painter: CirclePainter(
+            animationController,
+            color: kSpringColor
+          ),
+          child: Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(widget.size),
+              child: widget.child,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/gpsTrackingScreen.dart
+++ lib/screens/gpsTrackingScreen.dart
@@ -9,6 +9,7 @@ import '../providers/bluetoothProvider.dart';
 import '../utils/enums.dart';
 import '../providers/locationStatusProvider.dart';
 import '../providers/gpsTrackProvider.dart';
+import '../widgets/gpsTrackingWidgets/statusWidget.dart';
 import '../widgets/gpsTrackingWidgets/detailsIcon.dart';
 import '../widgets/gpsTrackingWidgets/statusIndicatorWidget.dart';
 import '../widgets/gpsTrackingWidgets/timeWidget.dart';
@@ -53,170 +54,187 @@ class _GpsTrackingScreenState extends State<GpsTrackingScreen> {
     }
 
     final bool showMap = gpsTrackProvider.locationDetermined;
+    final locationStatusProvider = Provider.of<LocationStatusProvider>(context);
+    final bluetoothProvider = Provider.of<BluetoothProvider>(context);
 
     return Scaffold(
       key: navigatorKey,
       body: showMap ? Stack(
         children: [
-          GoogleMap(
-            myLocationButtonEnabled: false,
-            myLocationEnabled: true,
-            markers: gpsTrackProvider.getMarkers,
-            polylines: gpsTrackProvider.getPolyLines,
-            circles: gpsTrackProvider.getCircles,
-            zoomControlsEnabled: false,
-            initialCameraPosition: gpsTrackProvider.getCameraPosition,
-            onMapCreated: (GoogleMapController googleMapController) async {
-              _googleMapController.complete(googleMapController);
-              await gpsTrackProvider.setMapController(_googleMapController.future);
-              gpsTrackProvider.addMarkersAndCircles();
-            },
+          Visibility(
+            visible: locationStatusProvider.locationState == LocationStatus.enabled && bluetoothProvider.isConnected(),
+            child: GoogleMap(
+              myLocationButtonEnabled: false,
+              myLocationEnabled: true,
+              markers: gpsTrackProvider.getMarkers,
+              polylines: gpsTrackProvider.getPolyLines,
+              circles: gpsTrackProvider.getCircles,
+              zoomControlsEnabled: false,
+              initialCameraPosition: gpsTrackProvider.getCameraPosition,
+              onMapCreated: (GoogleMapController googleMapController) async {
+                _googleMapController.complete(googleMapController);
+                await gpsTrackProvider.setMapController(_googleMapController.future);
+                gpsTrackProvider.addMarkersAndCircles();
+              },
+            ),
           ),
-          Container(
-            margin: const EdgeInsets.only(right: 5, top: 25),
-            padding: const EdgeInsets.all(10),
-            alignment: Alignment.topRight,
-            child: Column(
-              children: [
-                ClipOval(
-                  child: Material(
-                    color: kSpringColor,
-                    child: InkWell(
-                      splashColor: kSecondaryColor,
-                      onTap: () async {
-                        await myLocation();
-                      },
-                      child: const SizedBox(
-                        width: 50,
-                        height: 50,
-                        child: Icon(Icons.my_location, color: kWhiteColor),
+          Visibility(
+            visible: locationStatusProvider.locationState == LocationStatus.enabled && bluetoothProvider.isConnected(),
+            child: Container(
+              margin: const EdgeInsets.only(right: 5, top: 25),
+              padding: const EdgeInsets.all(10),
+              alignment: Alignment.topRight,
+              child: Column(
+                children: [
+                  ClipOval(
+                    child: Material(
+                      color: kSpringColor,
+                      child: InkWell(
+                        splashColor: kSecondaryColor,
+                        onTap: () async {
+                          await myLocation();
+                        },
+                        child: const SizedBox(
+                          width: 50,
+                          height: 50,
+                          child: Icon(Icons.my_location, color: kWhiteColor),
+                        ),
                       ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Material(
-                  color: kSpringColor,
-                  child: InkWell(
-                    splashColor: kWhiteColor,
-                    onTap: () async {
-                      await zoomIn();
-                    },
-                    child: const SizedBox(
-                      width: 40,
-                      height: 40,
-                      child: Icon(Icons.add, color: kWhiteColor),
-                    ),
-                  ),
-                ),
-                const SizedBox(
-                  width: 40,
-                  height: 2,
-                ),
-                Material(
-                  color: kSpringColor,
-                  child: InkWell(
-                    splashColor: kWhiteColor,
-                    onTap: () async {
-                      await zoomOut();
-                    },
-                    child: const SizedBox(
-                      width: 40,
-                      height: 40,
-                      child: Icon(Icons.remove, color: kWhiteColor),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Container(
-            margin: EdgeInsets.fromLTRB(10, deviceHeight * 0.73, 10, 25),
-            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-            width: deviceWidth,
-            height: deviceHeight * 0.24,
-            decoration: BoxDecoration(
-              color: kSpringColor,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  'Track ${gpsTrackProvider.trackId}',
-                  style: const TextStyle(
-                      color: kWhiteColor,
-                      fontWeight: FontWeight.w600,
-                      fontSize: 20
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  children: [
-                    Column(
-                      children: [
-                        Consumer<LocationStatusProvider>(
-                            builder: (context, gpsStateProvider, child) {
-                              final bool locationEnabled = gpsStateProvider.locationState == LocationStatus.enabled ? true : false;
-
-                              return StatusIndicatorWidget(
-                                title: 'gps'.toUpperCase(),
-                                icon: Icon(
-                                  locationEnabled ? Icons.location_on : Icons.location_off,
-                                  color: kSpringColor,
-                                ),
-                                backgroundColor: kWhiteColor,
-                              );
-                            }
-                        ),
-                        const SizedBox(height: 5),
-                        Consumer<BluetoothProvider>(
-                            builder: (context, bluetoothProvider, child) {
-                              final bool connectedToOBD = bluetoothProvider.isConnected();
-
-                              return StatusIndicatorWidget(
-                                title: 'Bluetooth',
-                                icon: Icon(
-                                  connectedToOBD ? Icons.bluetooth : Icons.bluetooth_disabled,
-                                  color: connectedToOBD ? kWhiteColor : kSpringColor,
-                                ),
-                                backgroundColor: connectedToOBD ? kBlueColor : kWhiteColor,
-                              );
-                            }
-                        ),
-                      ],
-                    ),
-                    const Spacer(),
-                    TimeWidget(
-                      duration: gpsTrackProvider.getTrackDuration,
-                      function: () {
-                        gpsTrackProvider.stopTrack();
-                        Navigator.of(context).pop();
+                  const SizedBox(height: 20),
+                  Material(
+                    color: kSpringColor,
+                    child: InkWell(
+                      splashColor: kWhiteColor,
+                      onTap: () async {
+                        await zoomIn();
                       },
+                      child: const SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: Icon(Icons.add, color: kWhiteColor),
+                      ),
                     ),
-                    const Spacer(),
-                    Column(
-                      children: const [
-                        DetailsIcon(
-                          title: 'Avg speed',
-                          data: '40 km/h',
-                          iconData: Icons.speed,
-                        ),
-                        SizedBox(
-                          height: 7,
-                        ),
-                        DetailsIcon(
-                          title: 'Distance',
-                          data: '1 km',
-                          iconData: Icons.trending_up_rounded,
-                        ),
-                      ],
+                  ),
+                  const SizedBox(
+                    width: 40,
+                    height: 2,
+                  ),
+                  Material(
+                    color: kSpringColor,
+                    child: InkWell(
+                      splashColor: kWhiteColor,
+                      onTap: () async {
+                        await zoomOut();
+                      },
+                      child: const SizedBox(
+                        width: 40,
+                        height: 40,
+                        child: Icon(Icons.remove, color: kWhiteColor),
+                      ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                ],
+              ),
             ),
           ),
+          Visibility(
+            visible: locationStatusProvider.locationState == LocationStatus.enabled && bluetoothProvider.isConnected(),
+            child: Container(
+              margin: EdgeInsets.fromLTRB(10, deviceHeight * 0.73, 10, 25),
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              width: deviceWidth,
+              height: deviceHeight * 0.24,
+              decoration: BoxDecoration(
+                color: kSpringColor,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Track ${gpsTrackProvider.trackId}',
+                    style: const TextStyle(
+                        color: kWhiteColor,
+                        fontWeight: FontWeight.w600,
+                        fontSize: 20
+                    ),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      Column(
+                        children: [
+                          Consumer<LocationStatusProvider>(
+                              builder: (context, gpsStateProvider, child) {
+                                final bool locationEnabled = gpsStateProvider.locationState == LocationStatus.enabled ? true : false;
+
+                                return StatusIndicatorWidget(
+                                  title: 'gps'.toUpperCase(),
+                                  icon: Icon(
+                                    locationEnabled ? Icons.location_on : Icons.location_off,
+                                    color: kSpringColor,
+                                  ),
+                                  backgroundColor: kWhiteColor,
+                                );
+                              }
+                          ),
+                          const SizedBox(height: 5),
+                          Consumer<BluetoothProvider>(
+                              builder: (context, bluetoothProvider, child) {
+                                final bool connectedToOBD = bluetoothProvider.isConnected();
+
+                                return StatusIndicatorWidget(
+                                  title: 'Bluetooth',
+                                  icon: Icon(
+                                    connectedToOBD ? Icons.bluetooth : Icons.bluetooth_disabled,
+                                    color: connectedToOBD ? kWhiteColor : kSpringColor,
+                                  ),
+                                  backgroundColor: connectedToOBD ? kBlueColor : kWhiteColor,
+                                );
+                              }
+                          ),
+                        ],
+                      ),
+                      const Spacer(),
+                      TimeWidget(
+                        duration: gpsTrackProvider.getTrackDuration,
+                        function: () {
+                          gpsTrackProvider.stopTrack();
+                          Navigator.of(context).pop();
+                        },
+                      ),
+                      const Spacer(),
+                      Column(
+                        children: const [
+                          DetailsIcon(
+                            title: 'Avg speed',
+                            data: '40 km/h',
+                            iconData: Icons.speed,
+                          ),
+                          SizedBox(
+                            height: 7,
+                          ),
+                          DetailsIcon(
+                            title: 'Distance',
+                            data: '1 km',
+                            iconData: Icons.trending_up_rounded,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          if (locationStatusProvider.locationState == LocationStatus.disabled && !bluetoothProvider.isConnected())
+            TabBarViewWidget()
+          else if (locationStatusProvider.locationState == LocationStatus.disabled)
+            LocationStatusWidget()
+          else if (!bluetoothProvider.isConnected())
+            BluetoothStatusWidget()
         ],
       ) : const Center(
         child: CircularProgressIndicator(),
