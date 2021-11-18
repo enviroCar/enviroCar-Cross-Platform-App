4429eb5706d7bd22ae92abeb0732fc5f2cdeeabe
khyati <sainikhyati2001@gmail.com>
2021-11-18 21:37:55 +0530

    upgrade dependencies and build tools

--- lib/widgets/mapWidget.dart
+++ lib/widgets/mapWidget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:geolocator/geolocator.dart';
 import 'package:flutter_map/flutter_map.dart';
-import 'package:latlong/latlong.dart';
+import 'package:latlong2/latlong.dart';
 
 class MapWidget extends StatefulWidget {
   final Function initializeLocation;
@@ -16,7 +16,7 @@ class MapWidget extends StatefulWidget {
 class _MapWidgetState extends State<MapWidget> {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder<Object>(
+    return StreamBuilder<Position>(
       stream: Geolocator.getPositionStream(),
       builder: (_, snapshot) {
         if (snapshot.hasData) {
