44527145c9487631e0745d79977a0c984127634f
khyati <sainikhyati2001@gmail.com>
2022-09-07 15:24:51 +0530

    add faqs screen and android build compileSdkVersion update

--- lib/constants.dart
+++ lib/constants.dart
@@ -33,6 +33,7 @@ const Color kGreyColor = Color.fromARGB(255, 23, 33, 43);
 const Color kSpringColor = Color.fromARGB(255, 0, 223, 165);
 const Color kPrimaryColor = Color(0xff000000);
 const Color kSecondaryColor = Color(0xffD8D8D8);
+const Color kOffWhiteColor = Color(0xffe6e6e6);
 const Color kErrorColor = Color.fromARGB(250, 255, 50, 64);
 const Color kWhiteColor = Color(0xffffffff);
 const Color kTertiaryColor = Color.fromARGB(200, 0, 0, 0);
